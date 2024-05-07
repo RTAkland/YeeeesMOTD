02fd45fd7b68cd24b8a0b357244cd5a3de329727
RTAkland <rtakland@outlook.com>
2024-05-07 13:18:51 +0800

    feat: add desc util

---
+++ src/main/java/cn/rtast/yeeeesmotd/utils/DescUtil.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2024 RTAkland
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+
+package cn.rtast.yeeeesmotd.utils
+
+import kotlin.random.Random
+
+object DescUtil {
+
+    private val buildInDesc = listOf(
+        "\$player 是吧！ 还不赶紧进来\n不然有你好果汁吃！",
+        "这是 \$player 的专属服务器呢~",
+        "逸一时误一世！ \$player 赶紧给我进来！"
+    )
+
+    fun getRndDesc(): String {
+        val rnd = Random.nextInt(0, buildInDesc.size)
+        return buildInDesc[rnd]
+    }
+}
