4fe2d5f74361a6db52d7fb8fe601d87532e00f7c
RTAkland <rtakland@outlook.com>
2024-05-08 11:12:15 +0800

    feat: update manager version to v2

--- src/main/java/cn/rtast/yeeeesmotd/utils/file/DescriptionManager.kt
+++ src/main/java/cn/rtast/yeeeesmotd/utils/file/DescriptionManager.kt
@@ -17,29 +17,41 @@
 
 package cn.rtast.yeeeesmotd.utils.file
 
+import cn.rtast.yeeeesmotd.YeeeesMOTD
+import cn.rtast.yeeeesmotd.entity.file.Description
+import com.google.gson.reflect.TypeToken
 import kotlin.random.Random
 
-class DescriptionManager : ICSVManager("description.csv") {
+class DescriptionManager : IJsonManager<MutableList<Description>>("description.json", mutableListOf<Description>()) {
 
-    private val buildInDesc = listOf(
+
+    private val buildIn = listOf(
         "\$player 是吧? 还不赶紧进来\n不然有你好果汁吃!",
         "这是 \$player 的专属服务器呢~",
         "逸一时误一世! \n\$player 赶紧给我进来!"
     )
 
-
-    fun randomBuildInDesc(): String {
-        val rnd = Random.nextInt(0, buildInDesc.size)
-        return buildInDesc[rnd]
+    private fun read(): MutableList<Description> {
+        val str = this.file.readText()
+        val type = object : TypeToken<MutableList<Description>>() {}.type
+        return YeeeesMOTD.gson.fromJson(str, type)
     }
 
-    fun randomDescription(): List<String>? {
-        val allDesc = this.readCSV()
+    private fun write(data: MutableList<Description>) {
+        val serData = YeeeesMOTD.gson.toJson(data)
+        this.file.writeText(serData)
+    }
+
+    fun randomBuildInDesc(): String {
+        return buildIn[Random.nextInt(0, buildIn.size)]
+    }
+
+    fun randomDescription(): Description? {
+        val allDesc = this.read()
+
         if (allDesc.isEmpty()) {
             return null
         }
-
         return allDesc[Random.nextInt(0, allDesc.size)]
     }
-
 }
---
+++ src/main/java/cn/rtast/yeeeesmotd/utils/file/SkinHeadManager.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2024 RTAkland
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+
+package cn.rtast.yeeeesmotd.utils.file
+
+import cn.rtast.yeeeesmotd.YeeeesMOTD
+import cn.rtast.yeeeesmotd.entity.file.Head
+import cn.rtast.yeeeesmotd.utils.SkinHeadUtil
+import com.google.gson.reflect.TypeToken
+import java.util.*
+import kotlin.concurrent.thread
+
+class SkinHeadManager : IJsonManager<MutableList<Head>>("heads.json", mutableListOf()) {
+
+    private fun read(): MutableList<Head> {
+        val str = this.file.readText()
+        val type = object : TypeToken<MutableList<Head>>() {}.type
+        return YeeeesMOTD.gson.fromJson(str, type)
+    }
+
+    private fun write(data: MutableList<Head>) {
+        val serData = YeeeesMOTD.gson.toJson(data)
+        this.file.writeText(serData)
+    }
+
+    fun addHead(name: String, uuid: String, ip: String) {
+        thread {
+            val allHeads = this.read()
+            val skin = SkinHeadUtil.getSkinFavicon(uuid)
+            val encodedSkinImage = String(Base64.getEncoder().encode(skin), Charsets.UTF_8)
+            allHeads.add(Head(name, uuid, encodedSkinImage, ip))
+            this.write(allHeads)
+        }
+    }
+
+    fun getHead(ip: String): Head {
+        val allHeads = this.read()
+        return allHeads.find { it.ip == ip } ?: Head("", "", "", "")
+    }
+
+    fun exists(ip: String): Boolean {
+        val allHeads = this.read()
+        return allHeads.any { it.ip == ip }
+    }
+
+    fun clear() {
+        val allHeads = this.read()
+        allHeads.clear()
+        this.write(allHeads)
+    }
+}
