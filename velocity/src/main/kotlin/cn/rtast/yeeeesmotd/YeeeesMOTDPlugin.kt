41142a51ccec1ffbaeebbbefdd2a03047c95ea58
RTAkland <rtakland@outlook.com>
2024-05-16 17:19:36 +0800

    update: update code

--- velocity/src/main/kotlin/cn/rtast/yeeeesmotd/YeeeesMOTDPlugin.kt
+++ velocity/src/main/kotlin/cn/rtast/yeeeesmotd/YeeeesMOTDPlugin.kt
@@ -13,6 +13,8 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+
+
 package cn.rtast.yeeeesmotd
 
 import cn.rtast.yeeeesmotd.listeners.LoginEventListener
