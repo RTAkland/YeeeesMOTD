6f62cff3e8667b90e999821e9ad6c39c3beaa945
RTAkland <rtakland@outlook.com>
2024-05-17 13:18:18 +0800

    push

---
+++ platforms/fabric/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("fabric-loom") version("1.6-SNAPSHOT")
+}
+
+val pluginVersion: String by project
+val minecraftVersion: String by project
+val yarnMappings: String by project
+val loaderVersion: String by project
+val fabricVersion: String by project
+val fabricKotlinVersion: String by project
+
+group = "cn.rtast"
+version = pluginVersion
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:$minecraftVersion")
+    mappings("net.fabricmc:yarn:$yarnMappings:v2")
+    modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricVersion")
+
+    modImplementation("net.fabricmc:fabric-language-kotlin:$fabricKotlinVersion")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,15 @@
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "YeeeesMOTD"
 
 include(":core")
 include(":platforms:velocity")
+include(":platforms:fabric")
