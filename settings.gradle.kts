b50debbe23e740709627995cd4b87262799abf1f
RTAkland <rtakland@outlook.com>
2024-05-17 13:02:49 +0800

    fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,4 @@
-pluginManagement {
-    repositories {
-        maven {
-            name = "Fabric"
-            url = uri("https://maven.fabricmc.net/")
-        }
-        gradlePluginPortal()
-    }
-}
-
 rootProject.name = "YeeeesMOTD"
 
 include(":core")
 include(":platforms:velocity")
-//include(":platforms:bukkit")
-include(":platforms:fabric")
